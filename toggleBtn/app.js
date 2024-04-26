a9c360c3c79d870fa203d343f4f9b23fcef2b5a1
Asim Ali <142142845+asimalizamurani@users.noreply.github.com>
2024-04-26 11:50:47 +0500

    Toggle Button
    There is a a toggle button used to convert the body background form white to black and black to white.

---
+++ toggleBtn/app.js
@@ -0,0 +1,12 @@
+let body = document.querySelector('body');
+let circle = document.querySelector('.circle');
+let formBtn = document.querySelector('.form-btn');
+let name = document.querySelectorAll    ('.name');
+let compass = document.querySelectorAll ('.compass');
+
+function toggle() {
+    // alert("hello bro")
+    body.classList.toggle('dark');
+    circle.classList.toggle('move')
+    
+}
